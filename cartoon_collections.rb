b99dcf948c3ce79d432b4e5f833b122a4bf4c5a7
Donna Torres <todonnatorres@icloud.com>
2020-06-04 20:23:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|item|chesse_types.inlcude?(item)}
+  array.find {|item|cheese_types.inlcude?(item)}
 end
 
