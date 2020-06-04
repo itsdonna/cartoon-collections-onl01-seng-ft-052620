ac1eff6a939b20e4b77f980ef597f208340beb51
Donna Torres <todonnatorres@icloud.com>
2020-06-04 19:46:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 
-# def roll_call_dwarves(dwarves) 
-#   dwarves.each_with_index do | name, index |
-#     puts "#{index + 1} #{name}"
-#   end
-# end
+def roll_call_dwarves(dwarves) 
+  dwarves.each_with_index do | name, index |
+    puts "#{index + 1} #{name}"
+  end
+end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = ["carrot!", "cucumber!", "pepper!"]
