445ac860a49e3023c48cfe7bc22ff2900c162df5
Donna Torres <todonnatorres@icloud.com>
2020-06-04 19:46:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls = ["carrot!", "cucumber!", "pepper!"]
-  planeteer_calls.collect do |call|
+ planeteer_calls.collect do |call|
    call.capitalize + “!”
   end 
 end
